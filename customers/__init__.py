a6d7869d2c6cb5d3aff322a6b505cea5bc742d10
Shawn Conerton <conerton52@gmail.com>
2021-01-21 09:08:56 -0600

    chapter 10 complete

--- customers/__init__.py
+++ customers/__init__.py
@@ -1,1 +1,6 @@
-from .request import get_all_customers, get_single_customer, create_customer, delete_customer, update_customer
+from .request import get_all_customers
+from .request import get_single_customer
+from .request import create_customer
+from .request import delete_customer
+from .request import update_customer
+from .request import get_customers_by_email
